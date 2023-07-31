c42dbd29454372e04eb902ff5738c7eb97d4421c
ryoichi551124 <ryoichi551124@icloud.com>
2023-07-31 21:12:42 +0900

    コンポーネントの移動

--- resources/js/Components/Admin/Icon/DeleteLinkIcon.tsx
+++ resources/js/Components/Admin/Icon/DeleteLinkIcon.tsx
@@ -11,6 +11,10 @@ type DeleteLinkProps = {
 const icon = css`
   color: ${theme.colors.delete};
   margin: 0 0.5rem;
+  &:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s;
+  }
 `
 
 export default function DeleteLinkIcon({ deleteLink }: DeleteLinkProps) {
--- resources/js/Components/Admin/Icon/EditLinkIcon.tsx
+++ resources/js/Components/Admin/Icon/EditLinkIcon.tsx
@@ -11,6 +11,10 @@ type EditLinkProps = {
 const icon = css`
   color: ${theme.colors.secondary};
   margin: 0 0.5rem;
+  &:hover {
+    transform: scale(1.1, 1.1);
+    transition: 0.5s;
+  }
 `
 
 export default function EditLinkIcon({ editLink }: EditLinkProps) {
