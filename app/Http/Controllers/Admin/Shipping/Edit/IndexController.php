1c04e1ebd1bf3efa9dca18ee8acab302609052ed
ryoichi551124 <ryoichi551124@icloud.com>
2023-09-10 18:53:33 +0900

    コントローラーの修正

--- app/Http/Controllers/Admin/Shipping/Edit/IndexController.php
+++ app/Http/Controllers/Admin/Shipping/Edit/IndexController.php
@@ -21,21 +21,10 @@ class IndexController extends Controller
     {
         $shipping = Shipping::Id($request->route('id'))->first()->toArray();
 
-        // 郵便番号、電話番号は編集出来るよう分割する
-        $shipping_zip = array_values(split_zip($shipping['shipping_zip_code']));
-        $shipping_tel = array_values(split_tel($shipping['shipping_tel']));
         // 元のデータと結合
         $shipping = array_merge(
             $shipping,
-            [
-                'shipping_zip_code1' => $shipping_zip[0],
-                'shipping_zip_code2' => $shipping_zip[1]
-            ],
-            [
-                'shipping_tel1' => $shipping_tel[0],
-                'shipping_tel2' => $shipping_tel[1],
-                'shipping_tel3' => $shipping_tel[2],
-            ],
+            split_zip($shipping['zip_code']),
         );
 
         $user = User::Id($request->user_id)->first();
