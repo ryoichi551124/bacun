f77d4ae9f080ab6b80219e382b5ac836bf5637a5
ryoichi551124 <ryoichi551124@icloud.com>
2023-09-10 18:47:36 +0900

    リクエストクラスの修正

--- app/Http/Requests/Admin/Order/CreateRequest.php
+++ app/Http/Requests/Admin/Order/CreateRequest.php
@@ -26,20 +26,20 @@ class CreateRequest extends FormRequest
     {
         return [
             'user_id'           => ['required'],
-            'order_last_name'   => ['required', 'string', 'max:64'],
-            'order_first_name'  => ['required', 'string', 'max:64'],
-            'order_last_kana'   => ['required', 'string', 'max64'],
-            'order_first_kana'  => ['required', 'string', 'max:64'],
-            'order_zip_code'    => ['nullable', 'regex:/^\d{7}$/i'],
-            'order_pref'        => ['required', 'string', 'max:8'],
-            'order_city'        => ['required', 'string', 'max:32'],
-            'order_address'     => ['required', 'string', 'max:128'],
-            'order_building'    => ['nullable', 'string', 'max:128'],
-            'order_tel'         => ['nullable', 'regex:/^\d{10,11}$/i'],
-            'order_email'       => ['nullable', 'email', 'max:128'],
-            'order_sex'         => ['required', Rule::in(1, 2)],
-            'order_memo'        => ['nullable', 'string'],
-            'order_status'      => ['required', new UnsignedTinyInteger],
+            'last_name'         => ['required', 'string', 'max:64'],
+            'first_name'        => ['required', 'string', 'max:64'],
+            'last_kana'         => ['required', 'string', 'max64'],
+            'first_kana'        => ['required', 'string', 'max:64'],
+            'zip_code'          => ['nullable', 'regex:/^\d{7}$/i'],
+            'pref'              => ['required', 'string', 'max:8'],
+            'city'              => ['required', 'string', 'max:32'],
+            'address'           => ['required', 'string', 'max:128'],
+            'building'          => ['nullable', 'string', 'max:128'],
+            'tel'               => ['nullable', 'regex:/^\d{10,11}$/i'],
+            'email'             => ['nullable', 'email', 'max:128'],
+            'sex'               => ['required', Rule::in(1, 2)],
+            'memo'              => ['nullable', 'string'],
+            'status'            => ['required', new UnsignedTinyInteger],
             'tracking_number'   => ['nullable', 'string', 'max:128'],
             'subtotal'          => ['required', new UnsignedMediumInteger],
             'total_deliv_fee'   => ['required', new UnsignedMediumInteger],
@@ -59,20 +59,20 @@ class CreateRequest extends FormRequest
     {
         return [
             'user_id'           => 'ユーザーID',
-            'order_last_name'   => '姓',
-            'order_first_name'  => '名',
-            'order_last_kana'   => '姓カナ',
-            'order_first_kana'  => '名カナ',
-            'oder_zip_code'     => '郵便番号',
-            'order_pref'        => '都道府県',
-            'order_city'        => '市区町村',
-            'order_address'     => '丁目番地号',
-            'order_building'    => '建物名',
-            'order_tel'         => '電話番号',
+            'last_name'         => '姓',
+            'first_name'        => '名',
+            'last_kana'         => '姓カナ',
+            'first_kana'        => '名カナ',
+            'zip_code'          => '郵便番号',
+            'pref'              => '都道府県',
+            'city'              => '市区町村',
+            'address'           => '丁目番地号',
+            'building'          => '建物名',
+            'tel'               => '電話番号',
             'oder_eamil'        => 'メールアドレス',
-            'order_sex'         => '性別',
-            'order_memo'        => 'オーダーメモ',
-            'order_status'      => 'ステータス',
+            'sex'               => '性別',
+            'memo'              => 'オーダーメモ',
+            'status'            => 'ステータス',
             'tracking_number'   => 'トラッキングナンバー',
             'subtotal'          => '小計',
             'total_deliv_fee'   => '送料合計',
@@ -99,5 +99,18 @@ class CreateRequest extends FormRequest
             'date'          => ':attributeは無効な日付です',
             'in'            => ':attributeは無効な値です',
         ];
+    }
+
+    /**
+     * 誕生日、郵便番号、電話番号の結合
+     */
+    public function getValidatorInstance()
+    {
+        $input = $this->all();
+
+        // 郵便番号
+        $this->merge(combine_zip($input['zip_code1'], $input['zip_code2']));
+
+        return parent::getValidatorInstance();
     }
 }
