6b7e7c00b5a65afa03499e0a550fff8c4f8edb88
ryoichi551124 <ryoichi551124@icloud.com>
2023-10-24 22:16:26 +0900

    受注登録　リクエストクラスの作成　不要なリクエストクラスは削除

--- app/Http/Requests/Admin/Order/CreateRequest.php
+++ app/Http/Requests/Admin/Order/CreateRequest.php
@@ -25,28 +25,37 @@ class CreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_id'           => ['required'],
-            'last_name'         => ['required', 'string', 'max:64'],
-            'first_name'        => ['required', 'string', 'max:64'],
-            'last_kana'         => ['required', 'string', 'max64'],
-            'first_kana'        => ['required', 'string', 'max:64'],
-            'zip_code'          => ['nullable', 'regex:/^\d{7}$/i'],
-            'pref'              => ['required', 'string', 'max:8'],
-            'city'              => ['required', 'string', 'max:32'],
-            'address'           => ['required', 'string', 'max:128'],
-            'building'          => ['nullable', 'string', 'max:128'],
-            'tel'               => ['nullable', 'regex:/^\d{10,11}$/i'],
-            'email'             => ['nullable', 'email', 'max:128'],
-            'sex'               => ['required', Rule::in(1, 2)],
-            'memo'              => ['nullable', 'string'],
-            'status'            => ['required', new UnsignedTinyInteger],
-            'tracking_number'   => ['nullable', 'string', 'max:128'],
-            'subtotal'          => ['required', new UnsignedMediumInteger],
-            'total_deliv_fee'   => ['required', new UnsignedMediumInteger],
-            'tax'               => ['required', new UnsignedMediumInteger],
-            'total'             => ['required', new UnsignedMediumInteger],
-            'payment_method'    => ['required', 'string', 'max:64'],
-            'payment_date'      => ['nullable', 'date'],
+            // 注文者情報
+            'user_id'                       => ['required'],
+            'order_user.last_name'          => ['required', 'string', 'max:64'],
+            'order_user.first_name'         => ['required', 'string', 'max:64'],
+            'order_user.last_kana'          => ['required', 'string', 'max:64'],
+            'order_user.first_kana'         => ['required', 'string', 'max:64'],
+            'order_user.zip_code'           => ['nullable', 'regex:/^\d{7}$/i'],
+            'order_user.pref'               => ['required', 'string', 'max:8'],
+            'order_user.city'               => ['required', 'string', 'max:32'],
+            'order_user.address'            => ['required', 'string', 'max:128'],
+            'order_user.building'           => ['nullable', 'string', 'max:128'],
+            'order_user.tel'                => ['nullable', 'regex:/^\d{10,11}$/i'],
+            'order_user.email'              => ['nullable', 'email', 'max:128'],
+            'order_user.sex'                => ['required', Rule::in(1, 2)],
+            'order_user.memo'               => ['nullable', 'string'],
+            // 配送先情報
+            'order_shipping.last_name'      => ['required', 'string', 'max:64'],
+            'order_shipping.first_name'     => ['required', 'string', 'max:64'],
+            'order_shipping.last_kana'      => ['required', 'string', 'max:64'],
+            'order_shipping.first_kana'     => ['required', 'string', 'max:64'],
+            'order_shipping.zip_code'       => ['nullable', 'regex:/^\d{7}$/i'],
+            'order_shipping.pref'           => ['required', 'string', 'max:8'],
+            'order_shipping.city'           => ['required', 'string', 'max:32'],
+            'order_shipping.address'        => ['required', 'string', 'max:128'],
+            'order_shipping.building'       => ['nullable', 'string', 'max:128'],
+            'order_shipping.memo'           => ['nullable', 'string'],
+            // 購入商品
+            'orderDetails.*.product_id'     => ['required'],
+            'orderDetails.*.product_name'   => ['required', 'string', 'max:128'],
+            'orderDetails.*.price'          => ['required', new UnsignedMediumInteger],
+            'orderDetails.*.quantity'       => ['required', new UnsignedTinyInteger],
         ];
     }
 
@@ -58,28 +67,37 @@ class CreateRequest extends FormRequest
     public function attributes(): array
     {
         return [
-            'user_id'           => 'ユーザーID',
-            'last_name'         => '姓',
-            'first_name'        => '名',
-            'last_kana'         => '姓カナ',
-            'first_kana'        => '名カナ',
-            'zip_code'          => '郵便番号',
-            'pref'              => '都道府県',
-            'city'              => '市区町村',
-            'address'           => '丁目番地号',
-            'building'          => '建物名',
-            'tel'               => '電話番号',
-            'eamil'             => 'メールアドレス',
-            'sex'               => '性別',
-            'memo'              => 'オーダーメモ',
-            'status'            => 'ステータス',
-            'tracking_number'   => 'トラッキングナンバー',
-            'subtotal'          => '小計',
-            'total_deliv_fee'   => '送料合計',
-            'tax'               => '消費税',
-            'total'             => '合計',
-            'payment_method'    => '支払い方法',
-            'payment_date'      => '支払い日',
+            // 注文者情報
+            'user_id'                       => 'ユーザーID',
+            'order_user.last_name'          => '姓',
+            'order_user.first_name'         => '名',
+            'order_user.last_kana'          => '姓カナ',
+            'order_user.first_kana'         => '名カナ',
+            'order_user.zip_code'           => '郵便番号',
+            'order_user.pref'               => '都道府県',
+            'order_user.city'               => '市区町村',
+            'order_user.address'            => '丁目番地号',
+            'order_user.building'           => '建物名',
+            'order_user.tel'                => '電話番号',
+            'order_user.eamil'              => 'メールアドレス',
+            'order_user.sex'                => '性別',
+            'order_user.memo'               => 'オーダーメモ',
+            // 配送先情報
+            'order_shipping.last_name'      => '姓',
+            'order_shipping.first_name'     => '名',
+            'order_shipping.last_kana'      => '姓カナ',
+            'order_shipping.first_kana'     => '名カナ',
+            'order_shipping.zip_code'       => '郵便番号',
+            'order_shipping.pref'           => '都道府県',
+            'order_shipping.city'           => '市区町村',
+            'order_shipping.address'        => '丁目番地',
+            'order_shipping.building'       => '建物名',
+            'order_shipping.memo'           => '配送先メモ',
+            // 購入商品
+            'orderDetails.*.product_id'     => '商品ID',
+            'orderDetails.*.product_name'   => '商品名',
+            'orderDetails.*.price'          => '単価',
+            'orderDetails.*.quantity'       => '個数',
         ];
     }
 
@@ -96,20 +114,29 @@ class CreateRequest extends FormRequest
             'email'         => '不正なメールアドレスです',
             'max'           => ':attributeは:max文字以内で入力してください',
             'regex'         => ':attributeは不正な値です',
-            'date'          => ':attributeは無効な日付です',
-            'in'            => ':attributeは無効な値です',
         ];
     }
 
     /**
-     * 誕生日、郵便番号、電話番号の結合
+     * 郵便番号、電話番号の結合
      */
     public function getValidatorInstance()
     {
-        $input = $this->all();
+        $order_user = $this->order_user;
+        $order_shipping = $this->order_shipping;
 
-        // 郵便番号
-        $this->merge(combine_zip($input['zip_code1'], $input['zip_code2']));
+        // 注文者郵便番号、電話番号
+        $this->merge(['order_user' => array_merge(
+            $order_user,
+            combine_zip($order_user['zip_code1'], $order_user['zip_code2']),
+            combine_tel($order_user['tel1'], $order_user['tel2'], $order_user['tel3'])
+        )]);
+
+        // 配送先郵便番号
+        $this->merge(['order_shipping' => array_merge(
+            $order_shipping,
+            combine_zip($order_shipping['zip_code1'], $order_shipping['zip_code2'])
+        )]);
 
         return parent::getValidatorInstance();
     }
